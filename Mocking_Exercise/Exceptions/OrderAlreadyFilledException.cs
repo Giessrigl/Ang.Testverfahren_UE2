2bf65d6a6073df6717c704cc3e534c7b7221716b
Giessrigl <christian.giessrigl@gmail.com>
2021-05-23 01:00:50 +0200

    add implementation of order class

---
+++ Mocking_Exercise/Exceptions/OrderAlreadyFilledException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mocking_Exercise.Exceptions
+{
+    public class OrderAlreadyFilledException : Exception
+    {
+        public OrderAlreadyFilledException()
+        {
+
+        }
+
+        public OrderAlreadyFilledException(string message) : base(message)
+        {
+
+        }
+
+        public OrderAlreadyFilledException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
--- Mocking_Exercise/Order.cs
+++ Mocking_Exercise/Order.cs
@@ -1,4 +1,5 @@
-﻿using Mocking_Exercise.Interfaces;
+﻿using Mocking_Exercise.Exceptions;
+using Mocking_Exercise.Interfaces;
 using System;
 
 namespace Mocking_Exercise
@@ -11,8 +12,15 @@ namespace Mocking_Exercise
 
         public Order(string product, int amount)
         {
+            if (string.IsNullOrWhiteSpace(product))
+                throw new ArgumentNullException();
+
+            if (amount < 1)
+                throw new ArgumentOutOfRangeException();
+
             this.product = product;
             this.amount = amount;
+            this.IsFilled = false;
         }
 
         public bool IsFilled
@@ -23,12 +31,24 @@ namespace Mocking_Exercise
 
         public bool CanFillOrder(IWarehouse warehouse)
         {
-            throw new NotImplementedException();
+            if (warehouse.HasProduct(this.product))
+            {
+                if (warehouse.CurrentStock(product) >= this.amount)
+                    return true;
+            }
+
+            return false;
         }
 
         public void Fill(IWarehouse warehouse)
         {
-            throw new NotImplementedException();
+            if (this.IsFilled)
+                throw new OrderAlreadyFilledException();
+
+            if (CanFillOrder(warehouse))
+                warehouse.TakeStock(this.product, this.amount);
+
+            this.IsFilled = true;
         }
     }
 }
